8166b4ef07cf1068bf9da5b94b0ee442b8c9aa22
Danny <danny.hj.vogel@gmail.com>
2024-07-24 23:44:40 +0200

    Feat: add title

--- app.vue
+++ app.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+useHead({
+  title: "DV's Pokedex",
+  meta: [
+    {
+      name: "description",
+      content:
+        "A basic pokedex app using Nuxt.js for showing off the differences between SSR, CSR, and SSG.",
+    },
+  ],
+});
+</script>
+
 <template>
   <NuxtRouteAnnouncer />
   <NuxtLayout>
